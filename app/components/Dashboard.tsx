0f7e08f6a2934fdb1d23aab62b3e938cef29f7b5
Usaid <46266811+usaidpeerzada@users.noreply.github.com>
2024-01-02 00:02:06 +0530

    Update Dashboard.tsx

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -1,20 +1,11 @@
-// pages/index.js
 "use client";
 import { useState } from "react";
-import { unified } from "unified";
-import stream from "unified-stream";
-import remarkParse from "remark-parse";
-import remarkToc from "remark-toc";
-import remarkRehype from "remark-rehype";
-import rehypeDocument from "rehype-document";
-import rehypeFormat from "rehype-format";
-import rehypeStringify from "rehype-stringify";
-import ReactMarkdown from "react-markdown";
 import GeneratedDocument from "./GeneratedDocument";
 import SettingsModal from "./SettingsModal";
 import Select from "react-select";
-
 import { SlSettings } from "react-icons/sl";
+import LoadingSpinner from "./LoadingSpinner";
+import MarkdownEditor from "./MarkdownEditor";
 
 interface DashboardProps {
   initialData: { document: string };
@@ -27,9 +18,14 @@ export default function Dashboard({
   const [textCode, setTextCode] = useState("");
   const [generatedDocument, setGeneratedDocument] = useState(initialData);
   const [isSettingsOpen, setIsSettingsOpen] = useState(false);
-  const [selectedModel, setSelectedModel] = useState<string | null>(null);
+  const [selectedModel, setSelectedModel] = useState<string | null>(
+    "gemini-pro"
+  );
   const [message, setMessage] = useState("");
-  let apiKey: any;
+  const [isLoading, setIsLoading] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
+
+  let apiKey: string | any;
   if (typeof window !== "undefined") {
     apiKey =
       localStorage &&
@@ -42,28 +38,29 @@ export default function Dashboard({
       setMessage("Please enter an API key");
     }
     try {
-      const response = await fetch(
-        "http://192.168.1.6:8080/api/generate-document",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-          },
-          body: JSON.stringify({ githubRepo, textCode, apiKey, selectedModel }),
-        }
-      );
-
+      setIsLoading(true);
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({ githubRepo, textCode, apiKey, selectedModel }),
+      });
       const data = await response.json();
       setGeneratedDocument(data);
     } catch (error) {
       console.error("Error generating document:", error);
+    } finally {
+      setIsLoading(false);
     }
   };
+
   const modelOptions = [
-    // { value: "gpt-3.5-turbo", label: "GPT-3.5 Turbo" },
+    { value: "gpt-3.5-turbo", label: "GPT-3.5 Turbo" },
     { value: "gemini-pro", label: "Gemini Pro" },
   ];
+
   const customTheme = (theme: any) => ({
     ...theme,
     spacing: {
@@ -82,11 +79,7 @@ export default function Dashboard({
     }),
     control: (provided: any) => ({
       ...provided,
-      backgroundColor:
-        typeof window !== "undefined" &&
-        localStorage.getItem("darkMode") === "true"
-          ? "rgb(13 148 136)"
-          : "rgb(55 65 81)",
+      backgroundColor: "rgb(13 148 136)",
       borderRadius: "4px",
       outline: "none",
       border: "1px",
@@ -95,26 +88,15 @@ export default function Dashboard({
       ...provided,
       color: "#ffffff",
     }),
-    // container: (base: any) => ({
-    //   ...base,
-    // }),
     menuList: (base: any) => ({
       ...base,
-      // your desired height
     }),
   };
-  // const processedContent = unified()
-  //   .use(remarkParse)
-  //   .use(remarkToc)
-  //   .use(remarkRehype)
-  //   .use(rehypeDocument, { title: 'Contents' })
-  //   .use(rehypeFormat)
-  //   .use(rehypeStringify)
-  //   .processSync(generatedDocument);
+
   return (
-    <div className="min-h-screen p-6 flex text-white font-sans">
+    <div className="min-h-screen p-6 flex flex-wrap text-white font-sans">
       <div className="p-6 bg-light-dashboard text-light-primary dark:bg-gray-800 dark:text-gray-500 rounded-lg shadow-lg max-w-md w-full relative">
-        <div className="flex">
+        <div className="md:flex lg:flex xl:flex">
           <h1 className="text-3xl font-bold">📄 DocSkrive</h1>
           <div className="absolute right-20">
             <Select
@@ -141,15 +123,15 @@ export default function Dashboard({
           onClose={() => setIsSettingsOpen(false)}
           message={message}
           setMessage={setMessage}
+          darkMode={darkMode}
+          setDarkMode={setDarkMode}
         />
         <div className="mb-4 mt-4">
-          <label className="block text-sm font-medium mb-2">
-            GitHub Repository:
-          </label>
+          <label className="block text-sm font-medium mb-2">URL:</label>
           <input
             type="text"
             className="p-2 w-full bg-gray-100 dark:bg-gray-700 border border-gray-600 rounded focus:outline-none text-gray-900 dark:text-white placeholder-gray-500"
-            placeholder="Enter GitHub repository"
+            placeholder="Enter file url"
             value={githubRepo}
             onChange={(e) => setGithubRepo(e.target.value)}
           />
@@ -162,22 +144,29 @@ export default function Dashboard({
         <div className="mb-4">
           <label className="block text-sm font-medium mb-2">Text Code:</label>
           <textarea
-            className="p-2 w-full h-32 bg-gray-100 dark:bg-gray-700 border border-gray-600 rounded focus:outline-none text-gray-900 dark:text-white placeholder-gray-500"
+            className="p-2 w-full h-64 bg-gray-100 dark:bg-gray-700 border border-gray-600 rounded focus:outline-none text-gray-900 dark:text-white placeholder-gray-500"
             placeholder="Enter code"
             value={textCode}
             onChange={(e) => setTextCode(e.target.value)}
+            rows={500}
           />
         </div>
 
         <button
           className="bg-teal-600 dark:bg-gray-700 hover:bg-teal-800 text-gray-100 py-2 px-4 rounded-full transition-all duration-300 ease-in-out"
           onClick={generateDocument}
+          disabled={isLoading || textCode.length === 0}
         >
-          Generate Document
+          {isLoading ? <LoadingSpinner /> : "Generate"}
         </button>
       </div>
-      <div className="flex-1 pl-6">
-        <GeneratedDocument content={generatedDocument?.document} />
+      <div className="flex-1 mt-10 md:mt-0 lg:mt-0 xl:mt-0 md:pl-6 lg:pl-6">
+        <GeneratedDocument content={generatedDocument?.document}>
+          <MarkdownEditor
+            darkMode={darkMode}
+            initialData={{ content: generatedDocument?.document }}
+          />
+        </GeneratedDocument>
       </div>
     </div>
   );
