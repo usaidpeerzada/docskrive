768727a24fb25e6b3762e4defb2b752ed68fe991
Usaid <46266811+usaidpeerzada@users.noreply.github.com>
2024-01-02 00:02:31 +0530

    Update MarkdownEditor.tsx

--- app/components/MarkdownEditor.tsx
+++ app/components/MarkdownEditor.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import type { ContextStore } from "@uiw/react-md-editor";
-// No import is required in the WebPack.
 import "@uiw/react-md-editor/markdown-editor.css";
-// No import is required in the WebPack.
 import "@uiw/react-markdown-preview/markdown.css";
+import rehypeSanitize from "rehype-sanitize"; //preventing xss
 interface MarkdownProps {
   initialData: { content: string };
+  darkMode: boolean;
 }
 const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: false });
 type OnChange = (
@@ -14,7 +14,11 @@ type OnChange = (
   event?: React.ChangeEvent<HTMLTextAreaElement>,
   state?: ContextStore
 ) => void;
-export default function MarkdownEditor({ initialData }: MarkdownProps) {
+
+export default function MarkdownEditor({
+  darkMode,
+  initialData,
+}: MarkdownProps) {
   const [value, setValue] = React.useState(initialData?.content);
   const onChange = React.useCallback<OnChange>((val) => {
     setValue(val || "");
@@ -22,10 +26,15 @@ export default function MarkdownEditor({ initialData }: MarkdownProps) {
   return (
     <div className="container">
       <div data-color-mode="dark">
+        <div className="wmde-markdown"></div>
         <MDEditor
+          style={darkMode ? { backgroundColor: "rgb(17 24 39)" } : {}}
           height={600}
           value={value || initialData?.content}
           onChange={onChange}
+          previewOptions={{
+            rehypePlugins: [[rehypeSanitize]],
+          }}
         />
       </div>
     </div>
