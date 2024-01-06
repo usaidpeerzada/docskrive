2daee295ea0049f77ab1586ea5a55ff276f4d046
Usaid <46266811+usaidpeerzada@users.noreply.github.com>
2024-01-07 01:44:57 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,11 @@
 "use client";
-import Link from "next/link";
-import Dashboard from "./components/Dashboard";
+import { AnimatePresence } from "framer-motion";
+import LandingPage from "./components/LandingPage";
 
 export default function Page() {
-  const sampleInitialData = { document: "" };
-  ("min-h-screen p-6 flex bg-light-background text-light-primary font-sans dark:bg-gray-900 dark:text-white");
   return (
-    <div className="bg-light-background text-light-primary dark:bg-gray-900 dark:text-white">
-      <Dashboard initialData={sampleInitialData} />
-      <p className="text-center text-white dark:text-gray-600 pb-6">
-        <span>made by </span>
-        <Link href="https://www.usaid.dev" className="hover:underline">
-          usaid.dev
-        </Link>{" "}
-        |{" "}
-        <Link href="https://usaid.dev/contact" className="hover:underline">
-          contact
-        </Link>
-      </p>
-    </div>
+    <AnimatePresence mode="wait">
+      <LandingPage />
+    </AnimatePresence>
   );
 }
